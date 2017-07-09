9810f0423efb2a43580e642201236c53b0998853
WooTaeYoung <wooty9@naver.com>
2017-07-09 10:43:07 +0900

    판매점 리스트 액티비티 액션바에 뒤로가기 버튼 추가

--- app/src/main/java/com/example/hh/myeonmok/StoreListActivity.java
+++ app/src/main/java/com/example/hh/myeonmok/StoreListActivity.java
@@ -1,5 +1,6 @@
 package com.example.hh.myeonmok;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -16,7 +17,13 @@ public class StoreListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store_list);
+        /* 액션바 뒤로가기 버튼 */
+        ActionBar actionBar = getSupportActionBar();
 
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+
+        /* 코드 구현 부분 */
         store_list_recyclerView = (RecyclerView) findViewById(R.id.store_list_recycleView);
 
         // RecyclerView 의 아이템 레이아웃을 설정
